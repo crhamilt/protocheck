90939d2f84f8d3e844e802ba0d4eef8bc6f3d499
Craig Hamilton <crhamilt@gmail.com>
2016-11-03 12:29:40 -0400

    * handle strings and numbers for element values

--- protocheck.py
+++ protocheck.py
@@ -40,20 +40,24 @@ def protocheck(args):
 
     rownum = 0
     for line in reader:
+        print(line)
         if rownum > 0:
             key = line[0]
             stan = line[1]
             mn = line[2]
             mx = line[3]
-            specs[key] = tuple((float(stan), float(mn), float(mx)))
+            try:
+                specs[key] = tuple((float(stan), float(mn), float(mx)))
+            except:
+                specs[key] = tuple((stan.strip(), mn.strip(), mx.strip()))   # strings
         rownum += 1
 
     ifile.close()
 
     # verify the spec_file has ranges that encompass the standard value
     for value in list(specs.values()):
-        # value is a tuple (standard,min,max)
-        if (value[1] > value[0]) or (value[2] < value[0]):
+        # value is a tuple (standard,min,max), only check floats, not strings (such as PE direction)
+        if not isinstance(value[0],str) and (value[1] > value[0]) or (value[2] < value[0]):
             print(('bad spec file range:', value))
             sys.exit()
 
@@ -67,25 +71,52 @@ def protocheck(args):
     for key, value in list(specs.items()):
         print((key + '= ', value))
 
-    print('\nImage elements are:')
-    for key in list(specs.keys()):
-        print((key + '= ', eval('ds.' + key)))
+#    print('\nImage elements are:')
+#    for key in list(specs.keys()):
+#        print((key + '= ', eval('ds.' + key)))
 
     print('\nResults:\n')
-    print(('%18s %8s %8s %8s %8s %12s')
-          % ('Element', 'Value', 'Standard', 'Max', 'Min', 'Status'))
-    print(('%18s %8s %8s %8s %8s %12s')
-          % ('-------', '--------', '--------', '--------', '--------', '------------'))
+    print(('%30s %8s %8s %8s %8s %12s')
+          % ('Element', 'Value', 'Expected', 'Max', 'Min', 'Status'))
+    print(('%30s %8s %8s %8s %8s %12s')
+          % ('---------------------------', '--------', '--------', '--------', '--------', '------------'))
 
     for key, value in list(specs.items()):
-        imgval = float(eval('ds.' + key))
-        if (imgval >= value[1]) and (imgval <= value[2]):
-            status = "OK"
-        else:
-            status = "OutOfRange"
 
-        print(('%18s %8.2f %8.2f %8.2f %8.2f %12s')
-              % (key, imgval, value[0], value[1], value[2], status))
+        isfloat=False
+        # handle special cases first
+        if key=='PixelSpacingX':
+            ps = eval('ds.PixelSpacing')
+            val = float(ps[1])
+            isfloat = True
+        elif key == 'PixelSpacingY':
+            ps = eval('ds.PixelSpacing')
+            val = float(ps[0])
+            isfloat = True
+        elif isinstance(value[0],str):
+            val = eval('ds.' + key)
+        else:
+            val = float(eval('ds.' + key))
+            isfloat = True
+
+        if isfloat:
+            if (val >= value[1]) and (val <= value[2]):
+                status = "OK"
+            else:
+                status = "OutOfRange"
+        else:
+            if val == value[0]:
+                status = "OK"
+            else:
+                status = "OutOfRange"
+
+        if isfloat:
+            print(('%30s %8.2f %8.2f %8.2f %8.2f %12s')
+                  % (key, val, value[0], value[1], value[2], status))
+        else:
+            print(('%30s %8s %8s %8s %8s %12s')
+                % (key, val, value[0], value[1], value[2], status))
+
 
 
 if __name__ == "__main__":
